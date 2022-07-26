03249375a525abc0eb966698ff0fd35ff8035b41
wjddnrud <dnwk203@naver.com>
2022-07-26 17:14:25 +0900

    7/26수업

---
+++ src/com/HealthMate/study/ch02/Hello.java
@@ -0,0 +1,37 @@
+package com.HealthMate.study.ch02;
+
+public class Hello {
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		boolean abc = true;
+		boolean xyz;
+		xyz =  false;
+
+		int needs = 100;
+		String name = "정우경";
+		
+		
+		String category = "weight, diet, food";
+		String title = "단백질 보충제 얼마나 먹어야 하나요?";
+		String nickname = "헬린이";
+		String contents = "보충제 어쩌구 저쩌구~";
+		String comment = "2~3번 나눠 드시면 됩니다.";
+		String commentNickname = "백코치";
+		int commentCount = 7;
+		int viewCount = 15;
+		
+		System.out.println(name);
+		System.out.println("name 변수에 들어가 있는 값은 : " + name);
+		System.out.println(category);
+		System.out.println(title);
+		System.out.println(nickname);
+		System.out.println(contents);
+		System.out.println(comment);
+		System.out.println(commentNickname);
+		System.out.println(commentCount);
+		System.out.println(viewCount);
+		
+	}
+
+}
