5a1e9ac3804e8236fbb77f50a7d31e698793279c
wjddnrud <dnwk203@naver.com>
2022-07-28 17:32:38 +0900

    7/28수업

---
+++ src/com/HealthMate/study/ch02/CharExample.java
@@ -0,0 +1,37 @@
+package com.HealthMate.study.ch02;
+
+public class CharExample {
+
+	public static void main(String[] args) {
+		
+		
+		//자바는 모든 문자를 유니코드로 처리함.
+		//국제 표준 규약
+		//십진수 16진수 표가 따로 있으니 검색해서 확인가능
+		
+		char c1 = 'A';           //문자를 저장
+		char c2 = 65;            //십진수로 저장
+		char c3 = '\u0041';      //16진수로 저장
+		
+		System.out.println(c1);
+		System.out.println(c2);
+		System.out.println(c3);
+		
+		char c4 = '가';          //문자를 저장
+		char c5 = 44032;        //십진수로 저장
+		char c6 = '\uac00';     //16진수로 저장
+		
+		System.out.println(c4);
+		System.out.println(c5);
+		System.out.println(c6);
+		
+		int uniCode = c1;
+		System.out.println(uniCode);
+
+		int uniCode2 = c4;
+		System.out.println(uniCode2);
+		
+		
+	}
+
+}
