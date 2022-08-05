c3f802f938f2ecd1150bb4e0a4094fb5bd7e20f0
wjddnrud <dnwk203@naver.com>
2022-08-05 11:23:31 +0900

    8/5수업

---
+++ src/com/Ezem/study/ch03/AssignmentOperatorExample.java
@@ -0,0 +1,27 @@
+package com.Ezem.study.ch03;
+
+public class AssignmentOperatorExample {
+	public static void main(String[] args) {
+
+		int result = 0;
+		int result2 = 0;
+		int result3 = 0;
+		
+		result += 10;
+//		result = result + 10;
+		
+		System.out.println("result : " + result);
+		
+		result2 -= 10;
+//		result2 = result - 10;
+		
+		System.out.println("result2 : " + result2);
+		
+		result3 *= 10;
+//		result3 = result * 10;
+		
+		System.out.println("result3 : " + result3);
+		
+	}
+
+}
