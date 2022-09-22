389e05fb3fb56d2f5b0eac4487cb6b4c5d5f9e6c
wjddnrud <dnwk203@naver.com>
2022-09-22 17:58:52 +0900

    9/21 java Exception 수업

---
+++ src/com/Ezem/study/ch10/first/TryCatchFinallyExample.java
@@ -0,0 +1,14 @@
+package com.Ezem.study.ch10.first;
+
+public class TryCatchFinallyExample {
+
+	public static void main(String[] args) throws ClassNotFoundException {
+											// 여기에 throws Exception을 써주거나 밑에 try catch finally를 써주거나 하면 된다.
+//		try {
+			Class clazz = Class.forName("java.lang.String2");
+//		} catch(ClassNotFoundException e) {
+//			System.out.println("클래스가 존재하지 않습니다.");
+//		}
+	}
+
+}
